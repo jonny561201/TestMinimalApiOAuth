5ee6296eeb36ac1a187b51205c57b976e505d697
Jon Graf <jgraf@vermeer.com>
2025-05-22 12:54:42 -0500

    Initial creation of test project

---
+++ TestMinimalApi.Tests/Middleware/GlobalExceptionHandlerTests.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using TestMinimalAPI.Middleware;
+
+namespace TestMinimalApi.Tests.Middleware;
+
+public class GlobalExceptionHandlerTests
+{
+    private DefaultHttpContext _context;
+
+    public GlobalExceptionHandlerTests()
+    {
+        _context = new DefaultHttpContext();
+        _context.Response.Body = new MemoryStream();
+    }
+
+    [Fact]
+    public async Task Invoke_ShouldNotThrowWhenSuccessfulResponse()
+    {
+        RequestDelegate request = (HttpContext context) => Task.FromResult<object>(null);
+        var handler = new GlobalExceptionHandler(request);
+
+        await handler.Invoke(_context);
+        
+        Assert.Equal((int)HttpStatusCode.OK, _context.Response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Invoke_ShouldReturn400WhenBadRequestRaised()
+    {
+        RequestDelegate request = (HttpContext context) => throw new BadHttpRequestException("Fail Message");
+        var handler = new GlobalExceptionHandler(request);
+
+        await handler.Invoke(_context);
+
+        _context.Response.Body.Seek(0, SeekOrigin.Begin);
+        var response = await new StreamReader(_context.Response.Body).ReadToEndAsync();
+        Assert.Equal((int)HttpStatusCode.BadRequest, _context.Response.StatusCode);
+        Assert.Equal("Fail Message", response);
+    }
+}
