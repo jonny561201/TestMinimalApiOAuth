8bcfa3cc8869e70e0cdf22fa3b02df0bc3317987
Jon Graf <jgraf@vermeer.com>
2025-05-23 11:54:12 -0500

    initial attempt at getting a SPA config for .NET up and running

---
+++ TestMinimalAPI/Config/SpaConfiguration.cs
@@ -0,0 +1,30 @@
+using Microsoft.Extensions.FileProviders;
+
+namespace TestMinimalAPI.Config;
+
+public static class SpaConfiguration
+{
+    public static WebApplication ConfigSpa(this WebApplication app)
+    {
+
+        app.UseDeveloperExceptionPage()
+            .UseStaticFiles()
+            .UseRouting();
+        
+        var staticFilesPath = Path.Combine(app.Environment.ContentRootPath, "ClientApp", "dist");
+
+        app.UseDefaultFiles(new DefaultFilesOptions
+        {
+            FileProvider = new PhysicalFileProvider(staticFilesPath),
+            RequestPath = ""
+        });
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(staticFilesPath),
+            RequestPath = ""
+        });
+        
+        return app;
+    }
+}
