34aa1b3071392059cc4719ffe40f19936129c334
Jon Graf <jgraf@vermeer.com>
2025-05-30 13:11:49 -0500

    moved role "Test" to auth constants

--- TestMinimalAPI/Config/AuthConstants.cs
+++ TestMinimalAPI/Config/AuthConstants.cs
@@ -5,6 +5,11 @@ public static class AuthPolicies
     public const string TestUser = "TestUser";
 }
 
+public static class AuthRoles
+{
+    public const string Test = "Test";
+}
+
 public static class AuthClaims
 {
     public const string Roles = "https://testminimalapi.example.com/roles";
