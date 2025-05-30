7b334db2b95a5011fcb986c55f95a39224a25d9c
Jon Graf <jgraf@vermeer.com>
2025-05-30 16:41:53 -0500

    updated the service to query the database for a person by id

--- TestMinimalAPI/Services/TestService.cs
+++ TestMinimalAPI/Services/TestService.cs
@@ -1,14 +1,33 @@
-namespace TestMinimalAPI.Services.Services;
+using System.Net;
+using TestMinimalAPI.Data;
+using TestMinimalAPI.Data.Config;
+
+namespace TestMinimalAPI.Services;
 
 interface ITestService
 {
     string GetResponseText();
+    Task<Person> GetPerson(Guid id);
 }
 
 public class TestService : ITestService
 {
+    private readonly PersonDbContext _context;
+
+    public TestService(PersonDbContext context)
+    {
+        _context = context;
+    }
+    
     public string GetResponseText()
     {
         return "Success Test";
+    }
+
+    public async Task<Person> GetPerson(Guid id)
+    {
+        var person = await _context.People.FindAsync(id);
+
+        return person ?? throw new HttpRequestException("Record Not Found", null, HttpStatusCode.NotFound);
     }
 }
